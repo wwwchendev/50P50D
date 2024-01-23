ccba1fbd4cb3d6f88fbd91ce37f14b24222b8967
wwwchendev <wwwchendev@gmail.com>
2024-01-23 23:57:15 +0800

    feat✨: 完成day04項目

--- src/js/pages/day04.js
+++ src/js/pages/day04.js
@@ -1,7 +1,16 @@
 import projectSlider from '../components/projectSlider';
+import '../../stylesheet/page/_day04.scss';
 
 const day04 = () => {
   projectSlider(4);
+  const search = document.querySelector('.search');
+  const btn = document.querySelector('.btn');
+  const input = document.querySelector('.input');
+  btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    // input.focus()展開的時候會自動聚焦在input輸入框
+    input.focus();
+  });
 };
 
 export default day04;
