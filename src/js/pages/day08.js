26c70d85bd416b6ece57e7a57c1d73f6b848ff1f
wwwchendev <wwwchendev@gmail.com>
2024-01-24 08:55:49 +0800

    feat✨: 完成day08項目

--- src/js/pages/day08.js
+++ src/js/pages/day08.js
@@ -1,6 +1,19 @@
 import projectSlider from '../components/projectSlider';
+import '../../stylesheet/page/_day08.scss';
 
 const day08 = () => {
   projectSlider(8);
+  const labels = document.querySelectorAll('.form-control label');
+  labels.forEach((label) => {
+    const modifiedHTML = label.innerText
+      .split('')
+      .map(
+        (letter, idx) =>
+          `<span style="transition-delay:${idx * 50}ms">${letter}</span>`,
+      )
+      .join('');
+    label.innerHTML = modifiedHTML;
+  });
 };
+
 export default day08;
