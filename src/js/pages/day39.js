b16f76ec7f0f9478c6c8d9d681a1455f7654afd5
wwwchendev <wwwchendev@gmail.com>
2024-01-24 16:12:00 +0800

    feat✨: 完成day39項目

--- src/js/pages/day39.js
+++ src/js/pages/day39.js
@@ -3,6 +3,15 @@ import '../../stylesheet/page/_day39.scss';
 
 const day39 = () => {
   projectSlider(39);
+  const password = document.getElementById('password');
+  const background = document.getElementById('background');
+
+  password.addEventListener('input', (e) => {
+    const input = e.target.value;
+    const { length } = input;
+    const blurValue = 20 - length * 2;
+    background.style.filter = `blur(${blurValue}px)`;
+  });
 };
 
 export default day39;
