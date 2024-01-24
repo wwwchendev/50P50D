8b419a91ff010ab89dd6c87eeb6ac472afbb4a50
wwwchendev <wwwchendev@gmail.com>
2024-01-24 10:01:37 +0800

    feat✨: 完成day11項目

--- src/js/pages/day11.js
+++ src/js/pages/day11.js
@@ -3,6 +3,24 @@ import '../../stylesheet/page/_day11.scss';
 
 const day11 = () => {
   projectSlider(11);
+  const insert = document.getElementById('insert');
+  window.addEventListener('keydown', (e) => {
+    // console.log(e); // KeyboardEvent
+    insert.innerHTML = `
+    <div class="key">
+       ${e.key === ' ' ? 'Space' : e.key} 
+        <small>event.key</small>
+    </div>
+    <div class="key">
+        ${e.keyCode}
+        <small>event.keyCode(淘汰)</small>
+    </div>
+    <div class="key">
+        ${e.code}
+        <small>event.code</small>
+    </div>    
+    `;
+  });
 };
 
 export default day11;
