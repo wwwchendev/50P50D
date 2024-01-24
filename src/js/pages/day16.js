bc50fc3f555a85640220e8babe5546c89b5e91e7
wwwchendev <wwwchendev@gmail.com>
2024-01-24 10:58:06 +0800

    feat✨: 完成day16項目

--- src/js/pages/day16.js
+++ src/js/pages/day16.js
@@ -3,6 +3,60 @@ import '../../stylesheet/page/_day16.scss';
 
 const day16 = () => {
   projectSlider(16);
+  const remained = document.getElementById('remained');
+  const listers = document.getElementById('liters');
+  const percentage = document.getElementById('percentage');
+  const smallCups = document.querySelectorAll('.cup-small');
+
+  function highlightCups(idx) {
+    if (smallCups[idx].classList.contains('full')) {
+      if (
+        idx === smallCups.length - 1 ||
+        !smallCups[idx].nextElementSibling.classList.contains('full')
+      ) {
+        idx -= 1;
+      }
+    }
+
+    smallCups.forEach((cup, idx2) => {
+      if (idx2 <= idx) {
+        cup.classList.add('full');
+      } else {
+        cup.classList.remove('full');
+      }
+    });
+  }
+
+  function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    const totalCups = smallCups.length;
+    // console.log(fullCups);
+    if (fullCups === 0) {
+      percentage.style.visibility = 'hidden';
+      percentage.style.height = 0;
+    } else {
+      percentage.style.visibility = 'visible';
+      percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+      percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+    }
+
+    if (fullCups === totalCups) {
+      remained.style.visibility = 'hidden';
+      remained.style.height = 0;
+    } else {
+      remained.style.visibility = 'visible';
+      listers.innerText = `${(1 - fullCups / totalCups) * 2}L`;
+    }
+  }
+  updateBigCup();
+
+  smallCups.forEach((cup, idx) => {
+    // console.log(idx);
+    cup.addEventListener('click', () => {
+      highlightCups(idx);
+      updateBigCup();
+    });
+  });
 };
 
 export default day16;
