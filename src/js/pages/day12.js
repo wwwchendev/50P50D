174aa116ac90e6ba2b818008b6c51bad376a0aef
wwwchendev <wwwchendev@gmail.com>
2024-01-24 10:06:38 +0800

    feat✨: 完成day12項目

--- src/js/pages/day12.js
+++ src/js/pages/day12.js
@@ -3,6 +3,21 @@ import '../../stylesheet/page/_day12.scss';
 
 const day12 = () => {
   projectSlider(12);
+  // const faqs = document.querySelectorAll('.faq');
+  const toggles = document.querySelectorAll('.faq-toggle');
+
+  // toggles.forEach((toggle,idx)=>{
+  //     toggle.addEventListener('click',()=>{
+  //         // console.log(idx);
+  //         faqs[idx].classList.toggle('active');
+  //     })
+  // })
+
+  toggles.forEach((toggle) => {
+    toggle.addEventListener('click', () => {
+      toggle.parentNode.classList.toggle('active');
+    });
+  });
 };
 
 export default day12;
