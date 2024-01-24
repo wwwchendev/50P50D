3b7fd03eca826587e41987c832aa6d17c960349d
wwwchendev <wwwchendev@gmail.com>
2024-01-24 12:35:36 +0800

    feat✨: 完成day25項目

--- src/js/pages/day25.js
+++ src/js/pages/day25.js
@@ -3,6 +3,16 @@ import '../../stylesheet/page/_day25.scss';
 
 const day25 = () => {
   projectSlider(25);
+  const header = document.querySelector('header');
+
+  function fixNav() {
+    if (window.scrollY > header.offsetHeight + 150) {
+      header.classList.add('active');
+    } else {
+      header.classList.remove('active');
+    }
+  }
+  window.addEventListener('scroll', fixNav);
 };
 
 export default day25;
