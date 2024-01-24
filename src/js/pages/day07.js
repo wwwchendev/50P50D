c2fee7eef262b9e8a550f9e11c0f87ddb8a5de66
wwwchendev <wwwchendev@gmail.com>
2024-01-24 08:44:29 +0800

    feat✨: 完成day07項目

--- src/js/pages/day07.js
+++ src/js/pages/day07.js
@@ -1,7 +1,24 @@
 import projectSlider from '../components/projectSlider';
+import '../../stylesheet/page/_day07.scss';
 
 const day07 = () => {
   projectSlider(7);
+  const left = document.querySelector('.left');
+  const right = document.querySelector('.right');
+  const container = document.querySelector('.container');
+
+  left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+  });
+  left.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-left');
+  });
+  right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right');
+  });
+  right.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-right');
+  });
 };
 
 export default day07;
