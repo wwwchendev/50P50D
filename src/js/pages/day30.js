f3bfd4e3975047448c6bf2af9fa467858746bf9d
wwwchendev <wwwchendev@gmail.com>
2024-01-24 13:00:28 +0800

    feat✨: 完成day30項目

--- src/js/pages/day30.js
+++ src/js/pages/day30.js
@@ -3,6 +3,33 @@ import '../../stylesheet/page/_day30.scss';
 
 const day30 = () => {
   projectSlider(30);
+  const resultEl = document.querySelector('#result');
+  const textEl = document.querySelector('#text');
+  const speedEl = document.querySelector('#speed');
+  const submit = document.querySelector('#submit');
+  let text = `Hello(^ρ^)/`;
+  let speed = 500 / speedEl.value;
+  let idx = 1;
+  function writeText() {
+    resultEl.innerHTML = text.slice(0, idx);
+    idx += 1;
+    if (idx <= text.length) {
+      // 遞歸
+      setTimeout(writeText, speed);
+    } else if (idx > text.length) {
+      idx = 1;
+    }
+  }
+  textEl.focus();
+  writeText();
+
+  submit.addEventListener('click', () => {
+    if (textEl.value !== '') {
+      text = textEl.value;
+    }
+    speed = 300 / speedEl.value;
+    writeText();
+  });
 };
 
 export default day30;
