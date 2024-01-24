d44b1b2d3f6bef22ac3825b41e1d325fa427cb65
wwwchendev <wwwchendev@gmail.com>
2024-01-24 15:10:43 +0800

    feat✨: 完成day48項目

--- src/js/pages/day48.js
+++ src/js/pages/day48.js
@@ -3,6 +3,24 @@ import '../../stylesheet/page/_day48.scss';
 
 const day48 = () => {
   projectSlider(48);
+  const container = document.querySelector('.container');
+  const unsplashURL = 'https://source.unsplash.com/random/';
+  const rows = 5;
+  const columns = 3;
+  function getRandomNr() {
+    return Math.floor(Math.random() * 10) + 300;
+  }
+  function getRandomSize() {
+    return `${getRandomNr()}x${getRandomNr()}`;
+    // `325x361`
+  }
+
+  // 迴圈產生 rows * columns數量的圖片元素
+  for (let i = 0; i < rows * columns; i += 1) {
+    const img = document.createElement('img');
+    img.src = `${unsplashURL}${getRandomSize()}`;
+    container.appendChild(img);
+  }
 };
 
 export default day48;
