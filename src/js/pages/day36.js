a637fbfdb3c717d2b572fa7d40e162aa48f3fc60
wwwchendev <wwwchendev@gmail.com>
2024-01-24 13:49:18 +0800

    feat✨: 完成day36項目

--- src/js/pages/day36.js
+++ src/js/pages/day36.js
@@ -3,6 +3,33 @@ import '../../stylesheet/page/_day36.scss';
 
 const day36 = () => {
   projectSlider(36);
+  const container = document.getElementById('container');
+  const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+  const SQUARES = 500;
+
+  function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+  function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+  }
+  function removeColor(element) {
+    element.style.background = '#252525';
+    element.style.boxShadow = '0 0 2px #000';
+  }
+
+  for (let i = 0; i < SQUARES; i += 1) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor(square));
+
+    square.addEventListener('mouseout', () => removeColor(square));
+
+    container.appendChild(square);
+  }
 };
 
 export default day36;
