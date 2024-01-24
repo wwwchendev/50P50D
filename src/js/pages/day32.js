a9f22f41a32b7e0334f2ff2709ecd03ea6602176
wwwchendev <wwwchendev@gmail.com>
2024-01-24 13:10:46 +0800

    feat✨: 完成day32項目

--- src/js/pages/day32.js
+++ src/js/pages/day32.js
@@ -3,6 +3,32 @@ import '../../stylesheet/page/_day32.scss';
 
 const day32 = () => {
   projectSlider(32);
+  const toggles = document.querySelectorAll('.toggle');
+  const tainan = document.querySelector('#tainan');
+  const kaohsiung = document.querySelector('#kaohsiung');
+  const pingtung = document.querySelector('#pingtung');
+
+  function doTheTrick(theClickedOne) {
+    // console.log(theClickedOne) //<input type="checkbox" id="tainan" class="toggle" checked="">
+
+    // 當三個選項都勾選時
+    if (tainan.checked && kaohsiung.checked && pingtung.checked) {
+      if (tainan === theClickedOne) {
+        pingtung.checked = false;
+      }
+
+      if (kaohsiung === theClickedOne) {
+        tainan.checked = false;
+      }
+
+      if (pingtung === theClickedOne) {
+        kaohsiung.checked = false;
+      }
+    }
+  }
+  toggles.forEach((toggle) =>
+    toggle.addEventListener('change', (e) => doTheTrick(e.target)),
+  );
 };
 
 export default day32;
