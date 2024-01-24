844329d22332839bf46b853c514fbb46f9632865
wwwchendev <wwwchendev@gmail.com>
2024-01-24 15:01:00 +0800

    feat✨: 完成day45項目

--- src/js/pages/day45.js
+++ src/js/pages/day45.js
@@ -3,6 +3,17 @@ import '../../stylesheet/page/_day45.scss';
 
 const day45 = () => {
   projectSlider(45);
+  const openBtn = document.querySelector('.open-btn');
+  const closeBtn = document.querySelector('.close-btn');
+  const nav = document.querySelectorAll('.nav');
+
+  openBtn.addEventListener('click', () => {
+    nav.forEach((navEl) => navEl.classList.add('visible'));
+  });
+
+  closeBtn.addEventListener('click', () => {
+    nav.forEach((navEl) => navEl.classList.remove('visible'));
+  });
 };
 
 export default day45;
